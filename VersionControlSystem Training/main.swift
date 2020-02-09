ad0eedabc8d84e4bb15591b7891288f0e80405ca
imchino <chino618@me.com>
2020-02-09 14:05:55 +0900

    Initial Commit

---
+++ VersionControlSystem Training/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VersionControlSystem Training
+//
+//  Created by chino on 2020/02/09.
+//  Copyright © 2020 chino. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
