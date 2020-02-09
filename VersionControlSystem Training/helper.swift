388728a23c2856f37d428974eb79af86d472ec01
imchino <chino618@me.com>
2020-02-09 15:37:50 +0900

    いつでも関数を呼び出して、コンソール出力できるようにしました。

---
+++ VersionControlSystem Training/helper.swift
@@ -0,0 +1,13 @@
+//
+//  helper.swift
+//  VersionControlSystem Training
+//
+//  Created by chino on 2020/02/09.
+//  Copyright © 2020 chino. All rights reserved.
+//
+
+import Foundation
+
+public func greeting(_ message: String) {
+    print(message)
+}
